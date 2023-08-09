9688030e4678efc9c082e636ed86780a493df5ba
MMW <null.mmw@gmail.com>
2023-08-09 11:46:23 +0500

    Codebase Clean to add readability and scalability

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,13 @@
-import { Outlet, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import Body from "./Components/Body/Body";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
-import Error from "./Components/Error/Error";
-import About from "./Components/About/About";
-import Cart from "./Components/Cart/Cart";
-import RestaurantMenu from "./Components/RestaurantMenu/RestaurantMenu";
-import Contact from "./Components/Contact/Contact";
+import appRouter from "./routes/Routes";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <RouterProvider router={appRouter} />
     </>
   );
 }
-
-export const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/restaurant/:resID",
-        element: <RestaurantMenu />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/",
-        element: <Body />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-  },
-]);
 
 export default App;
